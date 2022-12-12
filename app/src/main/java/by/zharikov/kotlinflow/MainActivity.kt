e0c8d92878f285a048ef1bfb950b7758486e0eaa
dimaz <zharikovdima3@gmail.com>
2022-12-12 10:59:35 +0300

    work with normal flow

--- app/src/main/java/by/zharikov/kotlinflow/MainActivity.kt
+++ app/src/main/java/by/zharikov/kotlinflow/MainActivity.kt
@@ -2,10 +2,34 @@ package by.zharikov.kotlinflow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import by.zharikov.kotlinflow.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val mBinding get() = _binding!!
+    private val mainViewModel by lazy {
+        ViewModelProvider(this)[MainViewModel::class.java]
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+        lifecycleScope.launch {
+            mainViewModel.countDownTimer.collect { countDownTimer ->
+                mBinding.textTimer.text = countDownTimer.toString()
+            }
+        }
+
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
